52af861ef07cb3d9fe6fabc0bf712301b01c1f87
Quốc Đạt Hồ <17110117@student.hcmute.edu.vn>
2021-06-11 23:52:01 +0700

    update 1106

--- debug_server.py
+++ debug_server.py
@@ -2,5 +2,6 @@ import uvicorn
 from Final_main_API import app
 
 
+
 if __name__ == '__main__':
-    uvicorn.run("debug_server:app",host='10.170.0.2',port=4200,reload=True)
+    uvicorn.run("debug_server:app", host='10.170.0.11', port=4200, reload=True)
